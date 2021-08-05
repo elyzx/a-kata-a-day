086f6fca7c3f3806f71b87c39097353013625be2
Ellen Children <ellenchildren@icloud.com>
2021-08-05 18:34:05 +0200

    Create find-the-parity-outlier.js

---
+++ 6-kyu/find-the-parity-outlier.js
@@ -0,0 +1,7 @@
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+
+function findOutlier(integers){
+  let evenOutlier = integers.filter(number => number % 2 == 0);
+  let oddOutlier = integers.filter(number => number % 2 !== 0);
+  return evenOutlier.length == 1 ? evenOutlier[0] : oddOutlier[0];
+}
