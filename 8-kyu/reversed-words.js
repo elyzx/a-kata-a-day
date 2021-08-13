3b336f41dd6fba7ef3ead55e3bd70df50f2f1026
Ellen Children <ellenchildren@icloud.com>
2021-08-13 11:12:40 +0200

    Create reversed-words.js

---
+++ 8-kyu/reversed-words.js
@@ -0,0 +1,6 @@
+// Reversed Words
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+function reverseWords(str){
+  return str.split(' ').reverse().join(' ');
+}
