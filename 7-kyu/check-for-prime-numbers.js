a211acdfcadffbe25437e125674959576b6bf26d
Ellen Children <ellenchildren@icloud.com>
2021-08-18 14:00:22 +0200

    Create check-for-prime-numbers.js

---
+++ 7-kyu/check-for-prime-numbers.js
@@ -0,0 +1,15 @@
+// Check for prime numbers
+// In this kata you will create a function to check a non-negative input to see if it is a prime number.
+// The function will take in a number and will return True if it is a prime number and False if it is not.
+
+function isPrime(n) {
+  if (n < 2) { 
+    return false
+  }
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false
+    }
+  }
+  return true
+}
