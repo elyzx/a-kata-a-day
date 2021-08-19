e135d0afcc647898c1d68cdb62c4220621b36251
Ellen Children <ellenchildren@icloud.com>
2021-08-19 11:02:03 +0200

    Create grasshopper-messi-goals.js

---
+++ 8-kyu/grasshopper-messi-goals.js
@@ -0,0 +1,7 @@
+// Grasshopper - Messi Goals
+// Use variables to find the sum of the goals Messi scored in 3 competitions
+
+let laLigaGoals = 43;
+let championsLeagueGoals = 10;
+let copaDelReyGoals = 5;
+let totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
