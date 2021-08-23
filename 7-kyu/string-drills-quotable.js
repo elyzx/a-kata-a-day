83ca6eafb60d16287ad770a1ca066a72b4d5265d
Ellen Children <ellenchildren@icloud.com>
2021-08-23 10:12:29 +0200

    Create string-drills-quotable.js

---
+++ 7-kyu/string-drills-quotable.js
@@ -0,0 +1,6 @@
+// Thinkful - String Drills: Quotable
+// This function should take two string parameters: a person's name (name) and a quote of theirs (quote), and return a string attributing the quote to the person in the following format.
+
+function quotable(name, quote){
+  return `${name} said: "${quote}"`
+}
