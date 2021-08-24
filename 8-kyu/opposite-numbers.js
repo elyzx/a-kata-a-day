eabf8b6886603740a76ee97de6273c1a600f362f
Ellen Children <ellenchildren@icloud.com>
2021-08-24 15:58:47 +0200

    Create opposite-numbers.js

---
+++ 8-kyu/opposite-numbers.js
@@ -0,0 +1,16 @@
+// Opposite numbers
+// Very simple, given a number, find its opposite.
+
+function opposite(number) {
+  if (number[0] != '-') {
+    return -number
+  }
+  else {
+    return number.trim[0]
+  }
+}
+
+// short version
+function opposite(number) {
+  return -number
+}
