70fa7cd530a5c061a3d7561009bded1ea33a9146
Ellen Children <ellenchildren@icloud.com>
2021-08-28 18:22:23 +0200

    Create grasshopper-debug-say-hello.js

---
+++ 8-kyu/grasshopper-debug-say-hello.js
@@ -0,0 +1,12 @@
+// Grasshopper - Debug sayHello
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Before
+function sayHello(name) {
+  return 'Hello'
+}
+
+// After
+const sayHello = (name) => {
+  return `Hello, ${name}`;
+}
