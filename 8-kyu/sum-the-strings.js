eb4be82c20525979bc65faf359ceb8a50308e470
Ellen Children <ellenchildren@icloud.com>
2021-09-02 09:44:53 +0200

    Create sum-the-strings.js

---
+++ 8-kyu/sum-the-strings.js
@@ -0,0 +1,6 @@
+// Sum The Strings
+// Create a function that takes 2 nonnegative integers in form of a string as an input, and outputs the sum (also as a string):
+
+const sumStr = (a,b) => {
+  return String(Number(a)+Number(b));
+}
