c78120412bac2ffc428bd50b0f90d4d47299f337
Ellen Children <ellenchildren@icloud.com>
2021-09-04 10:31:07 +0200

    Create you-only-need-one.js

---
+++ 8-kyu/you-only-need-one.js
@@ -0,0 +1,6 @@
+// You only need one - Beginner
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+const check = (a, x) => {
+  return a.includes(x);
+}
