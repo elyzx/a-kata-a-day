86a6ae75f676f9b5ec0da5f0ac63f7ad54d8c8f7
Ellen Children <ellenchildren@icloud.com>
2021-09-05 20:52:17 +0200

    Create are-you-playing-banjo.js

---
+++ 8-kyu/are-you-playing-banjo.js
@@ -0,0 +1,11 @@
+// Are You Playing Banjo?
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+function areYouPlayingBanjo(name) {
+  if (name[0] === 'R' || name[0] === 'r') {
+    return `${name} plays banjo`;
+  } else {
+    return `${name} does not play banjo`;
+  }
+}
