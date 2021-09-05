aa4d6381d8c80f8ba92646bd4b26343f119663f1
Ellen Children <ellenchildren@icloud.com>
2021-09-05 20:59:10 +0200

    Create a-needle-in-the-haystack.js

---
+++ 8-kyu/a-needle-in-the-haystack.js
@@ -0,0 +1,7 @@
+// A Needle in the Haystack
+// Can you find the needle in the haystack?
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+function findNeedle(haystack) {
+  return 'found the needle at position ' + haystack.indexOf('needle');
+}
