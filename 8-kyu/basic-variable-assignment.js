8b4994296808d9db6ac0986aaada6cf7547cf335
Ellen Children <ellenchildren@icloud.com>
2021-09-05 08:33:30 +0200

    Create basic-variable-assignment.js

---
+++ 8-kyu/basic-variable-assignment.js
@@ -0,0 +1,12 @@
+// Basic variable assignment
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+// Before
+var a == "code";
+var b == "wa.rs";
+var name == a + b;
+
+After 
+let a = "code";
+let b = "wa.rs";
+let name = a + b;
