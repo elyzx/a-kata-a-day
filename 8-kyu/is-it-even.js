177430089576587071cfda269f49bd95d06232c0
Ellen Children <ellenchildren@icloud.com>
2021-09-11 11:12:49 +0200

    Create is-it-even.js

---
+++ 8-kyu/is-it-even.js
@@ -0,0 +1,8 @@
+// Is it even?
+// In this Kata we are passing a number (n) into a function.
+// Your code will determine if the number passed is even (or not).
+// The function needs to return either a true or false.
+
+function testEven(n) {
+  return n % 2 === 0;
+}
