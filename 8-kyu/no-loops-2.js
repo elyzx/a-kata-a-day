83d8a83015598c989d9a5852d0bb2dd03e61920c
Ellen Children <ellenchildren@icloud.com>
2021-09-13 10:29:57 +0200

    Create no-loops-2.js

---
+++ 8-kyu/no-loops-2.js
@@ -0,0 +1,7 @@
+// No Loops 2 - You only need one
+// *** No Loops Allowed ***
+// You will be given an array (a) and a value (x). All you need to do is check whether the provided array contains the value, without using a loop.
+
+function check(a, x) {
+  return a.includes(x);
+};
